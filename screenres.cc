b35c1c9bdb965ba3523fec0f4b4f9f25dc03dca0
davidmarkclements <huperekchuno@googlemail.com>
2015-06-04 18:40:37 +0100

    tidy up spacing

--- screenres.cc
+++ screenres.cc
@@ -22,19 +22,19 @@ using v8::FunctionTemplate;
 void init(Handle<Object>);
 
 NAN_METHOD(set) {
-	NanScope();
-	int code = changeRes(args[0]->Uint32Value(), args[1]->Uint32Value());
+  NanScope();
+  int code = changeRes(args[0]->Uint32Value(), args[1]->Uint32Value());
 
-	if (!code) {
-		NanReturnUndefined();
-	} else {
-	  NanReturnValue(NanNew<Number>(code));
-	}
+  if (!code) {
+  NanReturnUndefined();
+  } else {
+    NanReturnValue(NanNew<Number>(code));
+  }
 }
 
 NAN_METHOD(get) {
-	NanScope();
-	Resolution res = fetchRes();
+  NanScope();
+  Resolution res = fetchRes();
   Local<Array> arr = NanNew<Array>(2);
   arr->Set(0, NanNew<Number>(res.width));
   arr->Set(1, NanNew<Number>(res.height));
@@ -42,8 +42,8 @@ NAN_METHOD(get) {
 }
 
 void init(Handle<Object> exports) {
-	exports->Set(NanNew<String>("set"), NanNew<FunctionTemplate>(set)->GetFunction());
-	exports->Set(NanNew<String>("get"), NanNew<FunctionTemplate>(get)->GetFunction());
+  exports->Set(NanNew<String>("set"), NanNew<FunctionTemplate>(set)->GetFunction());
+  exports->Set(NanNew<String>("get"), NanNew<FunctionTemplate>(get)->GetFunction());
 }
 
 NODE_MODULE(screenres, init);
