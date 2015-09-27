23915d47456aaca767472233105578211e69cdad
Ionică Bizău <bizauionica@gmail.com>
2015-09-27 21:30:31 +0300

    Ported the C++ file to be compatible with the recent changes in Nan

--- screenres.cc
+++ screenres.cc
@@ -22,28 +22,28 @@ using v8::FunctionTemplate;
 void init(Handle<Object>);
 
 NAN_METHOD(set) {
-  NanScope();
-  int code = changeRes(args[0]->Uint32Value(), args[1]->Uint32Value());
+  Nan::HandleScope scope;
+  int code = changeRes(info[0]->Uint32Value(), info[1]->Uint32Value());
 
   if (!code) {
-  NanReturnUndefined();
+    info.GetReturnValue().SetUndefined();
   } else {
-    NanReturnValue(NanNew<Number>(code));
+    info.GetReturnValue().Set(Nan::New<Number>(code));
   }
 }
 
 NAN_METHOD(get) {
-  NanScope();
+  Nan::HandleScope scope;
   Resolution res = fetchRes();
-  Local<Array> arr = NanNew<Array>(2);
-  arr->Set(0, NanNew<Number>(res.width));
-  arr->Set(1, NanNew<Number>(res.height));
-  NanReturnValue(arr);
+  Local<Array> arr = Nan::New<Array>(2);
+  arr->Set(0, Nan::New<Number>(res.width));
+  arr->Set(1, Nan::New<Number>(res.height));
+  info.GetReturnValue().Set(arr);
 }
 
-void init(Handle<Object> exports) {
-  exports->Set(NanNew<String>("set"), NanNew<FunctionTemplate>(set)->GetFunction());
-  exports->Set(NanNew<String>("get"), NanNew<FunctionTemplate>(get)->GetFunction());
+NAN_MODULE_INIT (init) {
+  Nan::Set(target, Nan::New<String>("set").ToLocalChecked(), Nan::New<FunctionTemplate>(set)->GetFunction());
+  Nan::Set(target, Nan::New<String>("get").ToLocalChecked(), Nan::New<FunctionTemplate>(get)->GetFunction());
 }
 
 NODE_MODULE(screenres, init);
